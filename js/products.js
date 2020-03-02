2aa255fab049f786040522790913357c0d7a85e2
Margret Williams <margretw@andrew.cmu.edu>
2020-03-02 03:23:02 -0500

    initial cart working

--- js/products.js
+++ js/products.js
@@ -3,6 +3,8 @@ var categories = [ 'decorative pillows', 'bed pillows', 'poufs' ]
 
 var detail_page = "product_detail.html"
 
+
+
 var products = [
 
     {
@@ -296,7 +298,6 @@ function isProductDetailPage() {
     return urlString.toString().includes("product_detail")
 }
 
-
 function renderPage() {
     if (isIndexPage()) {
         createProductCards(bestSellers, "bestsellers-cards")
@@ -311,10 +312,144 @@ function renderPage() {
     if (isProductDetailPage()) {
         createProductDetail()
     }   
+
+    displayCart();
 }
 
 
+
+var shoppingCart = (function() {
+    cart = [];
+
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    function saveCart() {
+        sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+    }
+    if (sessionStorage.getItem("shoppingCart") != null) {
+        loadCart();
+    }
+
+    var obj = {};
+
+    obj.addItemToCart = function(name, price, count) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart[item].count ++;
+                saveCart();
+                return;
+            }
+        }
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    }
+
+    obj.setCountForItem = function(name, count) {
+        for(var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+    };
+
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart[item].count --;
+                if(cart[item].count === 0) {
+                    cart.splice(item, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    obj.removeItemFromCartAll = function(name) {
+        for(var item in cart) {
+            if(cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    obj.clearCart = function() {
+        cart = [];
+        saveCart();
+    }
+
+    obj.totalCount = function() {
+        var totalCount = 0;
+        for(var item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    obj.totalCart = function() {
+        var totalCart = 0;
+        for(var item in cart) {
+            totalCart += cart[item].price * cart[item].count;
+        }
+        return Number(totalCart.toFixed(2));
+    }
+
+    obj.listCart = function() {
+        var cartCopy = [];
+        for(i in cart) {
+            item = cart[i];
+            itemCopy = {};
+            for(p in item) {
+                itemCopy[p] = item[p];
+
+          }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy)
+        }
+        return cartCopy;
+    }
+
+    return obj;
+})();
+
+
+function displayCart() {
+    document.getElementById("cart-amt").innerHTML = shoppingCart.totalCount()
+}
+
+
+$('.add-to-cart').click(function(event) {
+    var productID = window.location.toString().split("#")[1]
+    var detailProduct = products.find(product => product.product_id == productID)
+
+    shoppingCart.addItemToCart(detailProduct.name, detailProduct.price, 1);
+
+    displayCart();
+});
+
+$('.clear-cart').click(function() {
+    shoppingCart.clearCart();
+    displayCart();
+});
+
+
+
 renderPage()
+
+
+
 
 
 
